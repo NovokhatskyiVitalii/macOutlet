415816dbdca304dcc2ccf8c4ed26c6d9d8a73500
Vitalii <tomrain78@gmail.com>
2022-07-21 14:54:35 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+function renderItem(item) {
+  let orderStock = item.orderInfo.inStock;
+  let imgUrl = "";
+  let btnClass = "";
+  let reviews = item.orderInfo.reviews;
+  let textReviews = "";
+  let ordersDone = Math.floor(Math.random() * 10000);
+
+  if (orderStock == 0) {
+    imgUrl = "img/icons/logo-check-noitems.svg"
+    btnClass = "card-btn disabled"
+  } else {
+    imgUrl = "img/icons/check-icon.svg"
+    btnClass = "card-btn"
+  }
+
+  if (reviews >= 50) {
+    textReviews = "Above average";
+  } else if (reviews <= 15) {
+    textReviews = "Shit";
+  } else {
+    textReviews = "Below average";
+  }
+
+  return `
+    <div class="item-card">
+      <img src="img/icons/like1.svg" class="like-img" width="20px" alt="like">
+
+      <div class="img-card">
+        <img src="img/${item.imgUrl}" alt="">
+      </div> 
+      <div class="card-info">
+        <h3>${item.name}</h3>
+        <div class="check-img">
+          <img src="${imgUrl}" alt="">
+          <div class="text color-txt-info">
+            <span class="str-text">${orderStock}</span> left in stock
+          </div>
+        </div>
+        <span class="color-txt-info">Price: <span class="str-text">${item.price}</span> $</span>
+        <button class="${btnClass}">Add to cart</button>
+      </div>
+      <div class="stats-card">
+        <div class="like-stats-img">
+          <img src="img/icons/likefull.svg" alt="">
+        </div>
+        <div class="stats-text color-txt-stats">
+          <span><span class="str-text">${reviews}%</span> Positive reviews</span>
+          <span>${textReviews}</span>
+        </div>
+        <div class="orders color-txt-stats">
+          <span class="str-text">${ordersDone}</span>
+          <span>orders</span>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function renderItems(itemsToRender) {
+  let itemsElement = document.getElementById('catalog-items');
+  let renderedItems = '';
+
+  itemsToRender.forEach(item => {
+    renderedItems += renderItem(item);
+  });
+
+  itemsElement.innerHTML = renderedItems;
+}
+
+renderItems(items);
